578cea192cda1dd9da54208c162cae0ad888e36a
Debony Schmidt <debonyschmidt@Debonys-MacBook-Pro.local>
2019-07-05 15:05:25 +1000

    Step 6 updates

---
+++ Base1/Step6_script.js
@@ -0,0 +1,5 @@
+function results() {
+	var first_number = document.getElementById ("first_number").value;
+	var second_number = document.getElementById ("second_number").value;
+	document.write ("The result is: "+(first_number/second_number));
+}
