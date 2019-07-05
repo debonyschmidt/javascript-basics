1d5057d3c8ead1cd0c603c2c45ca45b8c950164d
Debony Schmidt <debonyschmidt@Debonys-MacBook-Pro.local>
2019-07-05 16:16:56 +1000

    Step  8

---
+++ Base1/Step8_script.js
@@ -0,0 +1,8 @@
+function results() {
+	var age = document.getElementById ("age").value;
+	if (age > 18) {
+		document.write("You are over 18!");
+	} else {
+		document.write("You are under 18!");
+	}
+}
